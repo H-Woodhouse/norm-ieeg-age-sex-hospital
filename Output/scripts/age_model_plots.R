d04cfecda88a1e80c4c1ab94cf862b32f197ce8c
Heather Woodhouse (PGR) <h.woodhouse@newcastle.ac.uk>
2024-01-18 13:31:20 +0000

    Adding all new output scripts

---
+++ Output/scripts/age_model_plots.R
@@ -0,0 +1,179 @@
+################################################################################
+
+## Script to produce plots for the age model outputs
+## Requires outputs from scripts in the Modelling folder
+
+
+#### PRELIMINARIES #############################################################
+
+# packages
+library(tidyverse)                 # data frames & ggplot
+library(reshape)                   # melting data frames
+library(scales)                    # unit control for plots
+library(grid)                      # to combine plots
+library(conflicted)                # handle package clashes
+conflict_prefer("rename","dplyr")  # use rename from dplyr
+conflict_prefer("filter", "dplyr")
+theme_set(theme_minimal(base_size = 24))
+
+# import summaries
+setwd("/media/b6036780/8TB1/norm-ieeg-age-sex-site/Output/")
+mod_summaries = read.csv("age_ROI_stats.csv")
+CIs = read.csv("age_wholebrain_stats.csv")
+
+# load ROI info from DB
+ROI1_info=read.csv("/media/b6036780/8TB1/norm-ieeg-age-sex-site/Data/ROI1_info.csv")
+
+# colours
+fb_colors=c("#3366ff","#bf80ff","#ff00bf","#3de600","#00cccc")
+
+# bands
+band = c("delta","theta","alpha","beta","gamma")
+
+# greek letters
+fb_lab=c(expression(delta),expression(theta),expression(alpha),expression(beta),expression(gamma))
+
+
+
+#### INITIAL DATA FRAMES #######################################################
+
+# attach roi & ppr info to region level stats
+region_summaries=left_join(ROI1_info,mod_summaries)
+region_summaries_RH = na.omit(region_summaries) %>% 
+  arrange(no.pts) %>% 
+  mutate(Area=factor(Area,levels=Area))
+
+# create factor for whole brain
+CIs = CIs %>% mutate(Band=factor(Band, levels=Band))
+
+# clear work space
+rm(mod_summaries, ROI1_info)
+
+#### PATIENTS PER REGION PLOT ##################################################
+
+ppr=ggplot(data=region_summaries_RH) +
+  geom_col(aes(x=Area,y=no.pts), fill="grey") +ylab("No. of subjects in region") +
+  theme(axis.title.y=element_text(margin=margin(r=10)),axis.text.x=element_blank(),
+        axis.title.x = element_blank(),panel.grid.major.x = element_blank())
+
+
+
+#### COLOR GRID CI & SINGULARITY PLOT ##########################################
+
+#### DF CREATION
+
+region_CI = region_summaries_RH %>%
+  select(c("Area","no.pts",paste0(band,"_none0_CI"),paste0(band,"_singular")))
+
+# make different indicator for each fb (delta: 0s & 1s, theta: 0s and 2s, etc)
+# this enables plotting each band in a different colour 
+for (i in 2:5) {
+  region_CI[,paste0(band[i],"_none0_CI")] = region_CI[,paste0(band[i],"_none0_CI")]*(i)
+}
+
+# need to also include a singularity indicator in each fb column
+# if singular, CI indicator doesn't matter/is overwritten
+# assign -1 = singular, then there's still the 0 & n for the CI indicator
+region_CI = region_CI %>%
+  mutate(delta_none0_CI = if_else(delta_singular==1,-1,delta_none0_CI)) %>%
+  mutate(theta_none0_CI = if_else(theta_singular==1,-1,theta_none0_CI)) %>%
+  mutate(alpha_none0_CI = if_else(alpha_singular==1,-1,alpha_none0_CI)) %>%
+  mutate(beta_none0_CI  = if_else(beta_singular ==1,-1,beta_none0_CI))  %>%
+  mutate(gamma_none0_CI = if_else(gamma_singular==1,-1,gamma_none0_CI)) %>%
+  select(-c(paste0(band,"_singular")))
+
+# melt for suitable ggplot format and reformat new columns
+region_CI = melt(region_CI, id.vars=c("Area","no.pts"))
+region_CI = region_CI %>% 
+  rename(freqband=variable) %>% mutate(freqband=gsub("_none0_CI","",freqband)) %>%
+  mutate(freqband=factor(freqband,levels = band)) %>% rename(none0_CI=value) %>%
+  mutate(none0_CI=factor(none0_CI,levels=-1:5))
+
+#### PLOT CREATION 
+
+# factor level order is -1 (singular), 0 (CI has 0), 1:5 (CI is none 0)
+CI=ggplot(data = region_CI) +
+  geom_tile(aes(x=Area,y=freqband,fill=none0_CI),show.legend = F) +
+  xlab(NULL) + ylab(NULL) + scale_y_discrete(labels=rev(fb_lab), limits=rev) +
+  theme(axis.text.x=element_blank(),panel.grid.major.x=element_blank()) +
+  scale_fill_manual(values = c("black","white",fb_colors)) +
+  annotate("label", x=32.5,y=5,size=4.5,label="Shaded: CI does not contain 0\nWhite: CI contains 0\nBlack: Singular fit")
+
+
+
+#### STANDARD ERROR LINE PLOT ##################################################
+
+#### DF CREATION
+
+region_SE = region_summaries_RH %>%
+  select(c("Area","no.pts",paste0(band,"_SE")))
+
+# melt for suitable ggplot format and reformat resulting columns
+region_SE = melt(region_SE, id.vars=c("Area","no.pts"))
+region_SE = region_SE %>% 
+  rename(freqband=variable) %>% mutate(freqband=gsub("_SE","",freqband)) %>%
+  mutate(freqband=factor(freqband,levels = band)) %>% rename(SE=value)
+
+#### PLOT CREATION 
+
+SE=ggplot(data=region_SE) +
+  geom_line(aes(x=Area,y=SE, group=freqband,color=freqband)) +
+  ylab(expression(paste("Standard error of ",italic(b[age])))) + xlab("Region name") +
+  scale_y_continuous(labels = comma) + scale_color_manual(values = fb_colors, labels=fb_lab) +
+  theme(axis.text.x=element_text(hjust=0,angle=-90,vjust=0.5),
+        legend.title=element_blank(), legend.position = c(0.8,0.7), legend.background = element_rect(colour="grey"),
+        axis.title.x=element_text(margin=margin(t=10)),axis.title.y=element_text(margin=margin(r=10)),
+        panel.grid.major.x = element_blank())
+
+
+
+#### COMBINE! ##################################################################
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(ppr),ggplotGrob(CI),ggplotGrob(SE)))
+
+
+
+#### CONFIDENCE INTERVAL PLOT (WHOLE BRAIN) ####################################
+
+options(scipen=999) # no sci notation
+ggplot(data = CIs) +
+  geom_vline(xintercept = 0, linewidth = 2, color = "white") +
+  geom_vline(xintercept = 0, linetype = 2, col="gray30") +
+  geom_segment(mapping = aes(x=lower95ci,xend=upper95ci,y=Band, yend=Band), linewidth=2,lineend="butt") +
+  ylab(NULL) + xlab(expression(italic(b[age]))) + scale_y_discrete(limits=rev) +
+  theme(panel.grid.major.y = element_blank(),axis.text.y = element_blank(),axis.title.x=element_text(margin=margin(t=10))) + 
+  scale_x_continuous(breaks=c(-0.0008,-0.0004,0,0.0004,0.0008),limits = c(-0.0008,0.0008)) +
+  geom_point(aes(x=coeff,y=Band),size=5)
+
+
+
+#### MATLAB EXPORT FOR COEF MAPS ###############################################
+
+# df to export to create brain space visualisation in matlab
+# need to be mirrored for this
+
+# select stats for matlab (betas)
+beta_age = region_summaries %>%
+  select(c("ROI_name", "x", "y", "z", "Hemisphere","Cortical","Area",paste0(band,"_coef")))
+
+# mirror 
+empty_rows=as.integer(row.names(beta_age[beta_age$Hemisphere=="Left",]))
+for (i in empty_rows) {
+  if (beta_age$Cortical[i]=="SC" & beta_age$Area[i]==beta_age$Area[i+7]) {
+    beta_age[i,paste0(band,"_coef")]=beta_age[i+7,paste0(band, "_coef")]
+  }
+  else if (beta_age$Cortical[i]=="C" & beta_age$Area[i]==beta_age$Area[i+34]) {
+    beta_age[i,paste0(band,"_coef")]=beta_age[i+34,paste0(band,"_coef")]
+  }
+}
+
+# filter empty rows and give matlab names
+beta_age = beta_age %>% 
+  na.omit() %>%
+  rename(names=ROI_name) %>%
+  select(-c(Area,Hemisphere,Cortical))
+
+# save
+write.csv(beta_age, "sup-results/beta_age_coeffs_ROI1.csv", row.names=FALSE)
+
---
+++ Output/scripts/sup_age_range.R
@@ -0,0 +1,76 @@
+################################################################################
+
+# Script to check that the age range in each ROI is not affecting results
+# For Supplementary
+
+
+
+#### PRELIMINARIES #############################################################
+
+# packages
+library(tidyverse)            # data frames & ggplot
+library(ggpubr)               # add stats (corr coef) to ggplot
+library(gridExtra)            # combining ggplots
+
+# data
+setwd("/media/b6036780/8TB1/norm-ieeg-age-sex-site")
+BPdata_p = read.csv("Data/ROI1_relBP_pooled.csv")
+age_coeffs = read.csv("Output/age_ROI_stats.csv")
+
+band = c("delta","theta","alpha","beta","gamma")
+
+
+
+#### DATA FRAME CREATION #######################################################
+
+# calculate 0.05 and 0.95 percentiles and subtract to give rang/spread of
+# central 90% of ages
+ROI_ages = BPdata_p %>% 
+  group_by(ROI_R) %>%
+  reframe(age_range_l=quantile(Age,0.05), age_range_u=quantile(Age,0.95),
+          inter90range = age_range_u-age_range_l) %>%
+  select(ROI_R,inter90range)
+
+# select just coefs and ROI Index from ROI_stats df
+age_coeffs = age_coeffs %>%
+  select(ROI_index,paste0(band,"_coef")) %>%
+  rename(ROI_R=ROI_index)
+
+# combine
+ROI_ages = left_join(ROI_ages,age_coeffs)
+
+
+
+#### PLOTTING ##################################################################
+
+# plot settings
+options(scipen=999)           # no sci notation
+theme_set(theme_minimal(base_size = 20))
+
+# additions to all plots
+gg_all = list(
+  geom_point(), xlab("10-90th percentile age range"), ylab(expression(b[age])),
+  theme(plot.title = element_text(hjust = 0.5),panel.grid.minor.y = element_blank())
+)
+
+# each band 
+del=ggplot(data = ROI_ages, aes(x=inter90range,y=delta_coef)) + ggtitle(expression(delta*"-band")) +
+  stat_cor(method="spearman", label.x=45, label.y=-0.0021) + gg_all
+
+the=ggplot(data = ROI_ages, aes(x=inter90range,y=theta_coef)) + ggtitle(expression(theta*"-band")) +
+  stat_cor(method="spearman", label.x=45, label.y=-0.00065) + gg_all
+
+alp=ggplot(data = ROI_ages, aes(x=inter90range,y=alpha_coef)) + ggtitle(expression(alpha*"-band")) +
+  stat_cor(method="spearman", label.x=45, label.y=0.00015) + gg_all
+
+bet=ggplot(data = ROI_ages, aes(x=inter90range,y=beta_coef)) + ggtitle(expression(beta*"-band")) +
+  stat_cor(method="spearman", label.x=45, label.y=0.0011) + gg_all
+
+gam=ggplot(data = ROI_ages, aes(x=inter90range,y=gamma_coef)) + ggtitle(expression(gamma*"-band")) +
+  stat_cor(method="spearman", label.x=45, label.y=0.0008) + gg_all
+
+# combine
+pdf("Output/sup-results/ROI_ages.pdf", width=15,height=13)
+grid.arrange(del,the,alp,bet,gam)
+dev.off()
+
