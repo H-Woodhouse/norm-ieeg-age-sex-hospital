00efdeeeded80b403f8ae906396e4df01320d36c
Heather Woodhouse (PGR) <h.woodhouse@newcastle.ac.uk>
2024-07-16 14:45:41 +0100

    new meta plot with new db output

--- Output/scripts/EDA.R
+++ Output/scripts/EDA.R
@@ -7,13 +7,13 @@
 #### PRELIMINARIES #############################################################
 
 # packages 
-library(tidyverse)         # dataframes and ggplot
-library(forestplot)        # foest plot
+library(tidyverse)         # data frames and ggplot
+library(forestplot)        # forest plot
 library(gridExtra)         # combining plots
 
 # data
 setwd("/media/b6036780/8TB1/norm-ieeg-age-sex-site")
-BPdata_full = read.csv("Data/ROI1_relBP_full.csv")
+BPdata_full = read.csv("Data/Preprocessing/ROI1_RBP_full.csv")
 
 
 
@@ -22,13 +22,13 @@ BPdata_full = read.csv("Data/ROI1_relBP_full.csv")
 # visualise the age/sex distribution across sites
 
 # meta data 
-meta = BPdata_full %>% select(Patient_no,Site,Age,Sex) %>% 
-  distinct() %>% arrange(Patient_no)
+meta = BPdata_full %>% select(Pat_ID,Hospital,Age,Sex) %>% 
+  distinct() %>% arrange(Pat_ID)
 
 # basis stats
-N=length(unique(BPdata_full$Patient_no))
+N=length(unique(BPdata_full$Pat_ID))
 meta %>% count(Sex)
-meta %>% count(Site)
+meta %>% count(Hospital)
 min(meta$Age)
 max(meta$Age)
 mean(meta$Age)
@@ -46,7 +46,7 @@ site_full = c("Beijing Tiantan Hosp.", "Great Ormond St. Hosp.", "University of 
 
 # forest plot data set
 forest_df = meta %>%
-  group_by(Site) %>%
+  group_by(Hospital) %>%
   summarise(n=n(),across(Sex,list(M = ~ sum(. == "M"),F = ~ sum(. == "F"))),
             lower=min(Age),upper=max(Age),mean=mean(Age)) %>%
   unite(M_F, c(Sex_M,Sex_F), sep=":") %>% mutate(Site_full=site_full) %>%
@@ -58,9 +58,9 @@ forest_df |>
   forestplot(labeltext=c(Site_full,n, M_F), boxsize=0.25, xticks=seq(0,70,10), xlab="Age range with mean",
              zero=NA,fn.ci_norm=fpDrawDiamondCI,col=fpColors(lines="gray50")) |>
   fp_add_lines() |>
-  fp_add_header(Site_full="Site", n="N", M_F="M:F") |>
+  fp_add_header(Site_full="Hospital", n="N", M_F="M:F") |>
   fp_set_style(txt_gp=fpTxtGp(xlab=gpar(cex=1), ticks=gpar(cex=0.75)), align="lcc") |>
-  fp_append_row(Site_full="TOTAL",n=N,M_F="266:247",lower=4,upper=66,mean=31.51,is.summary = F)
+  fp_append_row(Site_full="TOTAL",n=N,M_F="261:241",lower=3,upper=66,mean=31.21,is.summary = F)
 dev.off()
 
 
@@ -70,12 +70,13 @@ dev.off()
 # see if there's evidence of a non linear relationship
 # included in supplementary
 theme_set(theme_minimal())
-del=ggplot(data=BPdata_full, aes(x=Age,y=deltaBP)) + geom_point(size=.5) + ylab(expression("RBP("*delta*")"))
-the=ggplot(data=BPdata_full, aes(x=Age,y=thetaBP)) + geom_point(size=.5) + ylab(expression("RBP("*theta*")"))
-alp=ggplot(data=BPdata_full, aes(x=Age,y=alphaBP)) + geom_point(size=.5) + ylab(expression("RBP("*alpha*")"))
-bet=ggplot(data=BPdata_full, aes(x=Age,y=betaBP )) + geom_point(size=.5) + ylab(expression("RBP("*beta*")"))
-gam=ggplot(data=BPdata_full, aes(x=Age,y=gammaBP)) + geom_point(size=.5) + ylab(expression("RBP("*gamma*")"))
+gg_all = list(geom_point(size=.5), theme(aspect.ratio=1))
+del=ggplot(data=BPdata_full, aes(x=Age,y=deltaBP)) + gg_all + ylab(expression("RBP("*delta*")"))
+the=ggplot(data=BPdata_full, aes(x=Age,y=thetaBP)) + gg_all + ylab(expression("RBP("*theta*")"))
+alp=ggplot(data=BPdata_full, aes(x=Age,y=alphaBP)) + gg_all + ylab(expression("RBP("*alpha*")"))
+bet=ggplot(data=BPdata_full, aes(x=Age,y=betaBP )) + gg_all + ylab(expression("RBP("*beta*")"))
+gam=ggplot(data=BPdata_full, aes(x=Age,y=gammaBP)) + gg_all + ylab(expression("RBP("*gamma*")"))
 
-pdf("Output/sup-results/linear_scatter_SUP.pdf", width = 9.5, height = 10)
+pdf("Output/sup-results/linear_scatter_SUP.pdf", width = 8, height = 10)
 grid.arrange(del,the,alp,bet,gam)
 dev.off()
