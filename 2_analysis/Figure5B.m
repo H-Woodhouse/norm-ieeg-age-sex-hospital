0f106ec33d5f5702682c4e26873f998e179a4ca4
Heather Woodhouse (PGR) <h.woodhouse@newcastle.ac.uk>
2024-11-07 17:53:08 +0000

    adapted to use the new overarching df which covers Fig5B (this script) and Fig 6

--- 2_analysis/Figure5B.m
+++ 2_analysis/Figure5B.m
@@ -1,41 +1,53 @@
-% % Script to produce the brain plots, which show regression 
-% % coefficients at the ROI level
-
-% % the function requires data in a specified format, this is 
-% % created and saved in 'age_model_plots.R'
-
-% % run for main results and also supplementary 
-
-
+% %
+% % Script to visualise regression coefficients at the ROI level
+% %
+% % FIGURE 5B:
+% % plot regional beta_age values on the brain (mirrored data is used but
+% % results are reflected across the midline)
+% %
+% % REQUIRE the data frame created and saved in 'regional_age_model_stats.R'
+% %
+% %
 %% directories and settings
 
-% working directory
-% check (main results or supp?)
-cd("/media/b6036780/8TB1/norm-ieeg-age-sex-site/3_output")
+% set working directory to script location (2_analysis)
+cd(fileparts(matlab.desktop.editor.getActive().Filename))
 
 % pipeline containing brain plotting functions
-path_plot_fn = '/media/b6036780/8TB1/norm-ieeg-age-sex-site/5_MATLAB/plotting/';
+path_plot_fn = '../MATLAB_lib/plotting/';
 addpath(genpath(path_plot_fn))
 
 % Fieldtrip for brain surface plots
-path_fieldtrip = '/media/b6036780/8TB1/norm-ieeg-age-sex-site/5_MATLAB/fieldtrip-20230215';  
+path_fieldtrip = '../MATLAB_lib/fieldtrip-20230215';  
 addpath(genpath(path_fieldtrip));
 rmpath(genpath([path_fieldtrip '/compat'])); % remove fieldtrip compatibility folder so don't duplicate matlab functions
 
-%% load data
+clear path_fieldtrip path_plot_fn
 
-% model summaries in required format from R
+%% load and format data
+
 bands = {'delta','theta','alpha','beta','gamma'};
-b_ages = readtable("regional_b_age_coeffs.csv");               % check file
-b_ages_matrix=table2array(b_ages(:,strcat(bands, '_coef')));
-title_labels = {'δ','θ','α','β','γ'};                          % for supp
 
+% load data
+regional_summaries = readtable('../3_output/regional_age_model_stats_TEST.csv','VariableNamingRule','preserve');
 
-% create atlas table (correct format for function)
-% in RIO1, 76 regions bc removed 3 ROI on each hemisphere
+% rename column to be what plotting function is looking for
+regional_summaries = renamevars(regional_summaries,"ROI_name","names");
+
+% retain just what's needed for plotting
+% (x,y,z,names,beta_age coefficients in each region and band)
+beta_ages = regional_summaries(:,["names","x","y","z",strcat(bands,"_coef")]);
+
+% convert coefficients to matrix for plotting function
+beta_ages_matrix=table2array(beta_ages(:,strcat(bands, '_coef')));
+
+% create coordinate/atlas table for plotting function
+% in ROI1, 76 regions because we removed 3 ROI on each hemisphere
 atlas_cortical=table;
-atlas_cortical.xyz=mat2cell([b_ages.x,b_ages.y,b_ages.z],76,3);
-atlas_cortical.names = mat2cell(b_ages.names,76);             % check number of ROIs
+atlas_cortical.xyz=mat2cell([beta_ages.x,beta_ages.y,beta_ages.z],76,3);
+atlas_cortical.names = mat2cell(beta_ages.names,76);       
+
+clear regional_summaries beta_ages
 
 %% plot maps and save (all FB)
 
@@ -44,15 +56,18 @@ atlas_cortical.names = mat2cell(b_ages.names,76);             % check number of 
 % once, delete empty fig 1, LEAVE incorrect fig 2 open then run again
 % resulting figure 1 is correct
 
+% title labels
+title_labels = {'δ','θ','α','β','γ'};
+
 % find limits for CLim (need symmetric)
-max(b_ages_matrix)
-min(b_ages_matrix)
+max(beta_ages_matrix)
+min(beta_ages_matrix)
 
 % plot
 % can show/hide band titles in function code
-vis_norm_map_on_brain_T(b_ages_matrix,bands,title_labels,atlas_cortical,'Colormap',bluewhitered(256), ...
+vis_norm_map_on_brain_T(beta_ages_matrix,bands,title_labels,atlas_cortical,'Colormap',bluewhitered(256), ...
     'CLim',[-0.0015,0.0015],'View',{'top'}, 'FontSize',20, ...
     'ColorbarLocation','westoutside','TitleFontSize',42)
 
 % save (too complex for vector format)
-saveas(gcf, 'regional_b_age_brains.png')
+saveas(gcf, '../3_output/regional_beta_age_brains.png')
